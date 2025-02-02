1c30048dc826646d8247bf8a1a7b7b3d874c94ec
Glitch (glitch-hello-node) <none>
2025-02-02 12:29:24 +0000

    💑🌽 Checkpoint ./public/style.css:70045378/92 ./public/index.html:70045378/251:70058459/135 ./public/script.js:70045378/636

--- public/script.js
+++ public/script.js
@@ -19,3 +19,6 @@ buttons.forEach(button => {
   button.addEventListener('mouseover', pauseAnimations); // Pause when hovering over a button
   button.addEventListener('mouseout', resumeAnimations); // Resume when mouse leaves the button
 });
+
+
+
