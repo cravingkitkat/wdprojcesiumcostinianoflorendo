cad952caf8e73dad971a5bbd767075b69c676c75
nixi <b2028nrjcostiniano@pshs.edu.ph>
2025-02-05 21:55:56 +0800

    Update script2.js

--- public/script2.js
+++ public/script2.js
@@ -1,45 +1,45 @@
-const slider = document.getElementById('sliding')
+const slider = document.getElementById('sliding') // Get nation container
 
-addEventListener('scroll', slide)
+addEventListener('scroll', slide) // Applies function upon scrolling
 function slide () {
 	let slidx
   let slidy
-  	slidx = scrollY / (window.innerWidth + 70) * 100
-    slidy = scrollY / window.innerHeight * 100
+  	slidx = scrollY / (window.innerWidth + 70) * 100 // Find amount to translate on x-axis
+    slidy = scrollY / window.innerHeight * 100 // Find amount to translate on y-axis
   
-	slider.style.transform = `translate(-${slidx}vw,${slidy}vh)`
+	slider.style.transform = `translate(-${slidx}vw,${slidy}vh)` // Translate container
 }
-addEventListener('scroll', invis)
-function invis () {
+addEventListener('scroll', invis) // Applies function upon scrolling
+function invis () { 
   let elementHeight = 80 * window.innerHeight / 100
   let invisible = (elementHeight - scrollY) / elementHeight
-  slider.style.opacity = invisible
+  slider.style.opacity = invisible // lowers opacity upon scrolling
 }
 
-const glider = document.getElementById('wheee')
+const glider = document.getElementById('wheee') // Get archon container
 
-addEventListener('scroll', fire)
+addEventListener('scroll', fire) // Applies function upon scrolling
 function fire () {
 	let slidex
   let slidey
-  	slidex = scrollY  / (window.innerWidth + 70) * 100
-    slidey = scrollY / window.innerHeight * 100
+  	slidex = scrollY  / (window.innerWidth + 70) * 100 // Find amount to translate on x-axis
+    slidey = scrollY / window.innerHeight * 100 // Find amount to translate on y-axis
   
-	glider.style.transform = `translate(-${slidex}vw,${slidey}vh)`
+	glider.style.transform = `translate(-${slidex}vw,${slidey}vh)` // Translate container
 } 
 
-addEventListener('scroll', noSee)
+addEventListener('scroll', noSee) // Applies function upon scrolling
 function noSee () {
   let eGrow = 80 * window.innerHeight / 100
   let cantSee = 1 - (eGrow - scrollY) / eGrow
-  glider.style.opacity = cantSee
+  glider.style.opacity = cantSee // raises opacity upon scrolling
 }
 
-const gleader = document.getElementById('see')
+const gleader = document.getElementById('see') // Get archon image container
 
-addEventListener('scroll', seeNo)
+addEventListener('scroll', seeNo) // Applies function upon scrolling
 function seeNo () {
   let eGrow = 80 * window.innerHeight / 100
   let cantSee = 1 - (eGrow - scrollY) / eGrow
-  gleader.style.opacity = cantSee
+  gleader.style.opacity = cantSee // raises opacity upon scrolling
 }
